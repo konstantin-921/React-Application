0d38ef8afa6e9d156ffe6baa64a847106bed507c
Kostya Podlesniy <konstantin-921@yandex.ru>
2018-06-02 17:29:46 +0300

    Fix loader 1.3

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -54,92 +54,77 @@ const config = {
       {
         test: /\.(css|scss)$/,
         exclude: /node_modules/,
-        use: [{
-          loader: 'css-loader',
-          options: {
-            modules: true,
-            importLoaders: 1,
-            sourceMap: true,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            'css-loader',
+            { loader: 'sass-loader', query: { sourceMap: false } },
+          ],
+          publicPath: '../',
+        }),
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192,
+              mimetype: 'image/png',
+              name: 'images/[name].[ext]',
+            },
           },
-        },
-        {
-          loader: 'postcss-loader',
-          options: {
-            sourceMap: true,
-          },
-        },
-        {
-          loader: 'sass-loader',
-          options: {
-            sourceMap: true,
-          },
-        },
         ],
-        publicPath: '../',
-      }),
       },
       {
-  test: /\.(png|jpg|gif)$/,
-    use: [
+        test: /\.eot(\?v=\d+.\d+.\d+)?$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'fonts/[name].[ext]',
+            },
+          },
+        ],
+      },
       {
-        loader: 'url-loader',
-        options: {
-          limit: 8192,
-          mimetype: 'image/png',
-          name: 'images/[name].[ext]',
-        },
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192,
+              mimetype: 'application/font-woff',
+              name: 'fonts/[name].[ext]',
+            },
+          },
+        ],
       },
-    ],
-      },
-{
-  test: /\.eot(\?v=\d+.\d+.\d+)?$/,
-    use: [
       {
-        loader: 'file-loader',
-        options: {
-          name: 'fonts/[name].[ext]',
-        },
+        test: /\.[ot]tf(\?v=\d+.\d+.\d+)?$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192,
+              mimetype: 'application/octet-stream',
+              name: 'fonts/[name].[ext]',
+            },
+          },
+        ],
       },
-    ],
-      },
-{
-  test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-    use: [
       {
-        loader: 'url-loader',
-        options: {
-          limit: 8192,
-          mimetype: 'application/font-woff',
-          name: 'fonts/[name].[ext]',
-        },
-      },
-    ],
-      },
-{
-  test: /\.[ot]tf(\?v=\d+.\d+.\d+)?$/,
-    use: [
-      {
-        loader: 'url-loader',
-        options: {
-          limit: 8192,
-          mimetype: 'application/octet-stream',
-          name: 'fonts/[name].[ext]',
-        },
-      },
-    ],
-      },
-{
-  test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-    use: [
-      {
-        loader: 'url-loader',
-        options: {
-          limit: 8192,
-          mimetype: 'image/svg+xml',
-          name: 'images/[name].[ext]',
-        },
-      },
-    ],
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192,
+              mimetype: 'image/svg+xml',
+              name: 'images/[name].[ext]',
+            },
+          },
+        ],
       },
     ],
   },
